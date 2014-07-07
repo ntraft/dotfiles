b3f92b7bb211776a0586fde4a9d364a2f73455d3
Neil Traft <ntraft@gmail.com>
2014-07-06 18:50:38 -0700

    Improved ROS setup scripts to better deal with machines where Hydro is not present or even any ROS distro.

--- conf/ros-setup.sh
+++ conf/ros-setup.sh
@@ -2,7 +2,19 @@
 if [ -f $HOME/.rosdir ]; then
 	ROSDIR=`cat $HOME/.rosdir`
 else
-	ROSDIR=/opt/ros/hydro
+	for DIR in `ls -r /opt/ros`; do
+		if [ -f /opt/ros/$DIR/setup.sh ]; then
+			ROSDIR=/opt/ros/$DIR
+			break
+		fi
+	done
+fi
+
+if [ ! -d "$ROSDIR" ]; then
+	echo "No ROS distribution found."
+	return 1
+else
+	echo "Using ROS distro at: $ROSDIR"
 fi
 
 # Environment variables for ROS.
