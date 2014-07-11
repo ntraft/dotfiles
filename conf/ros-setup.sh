4f370802ffdb9d311bf34d78d070cae7de7970e5
Neil Traft <ntraft@gmail.com>
2014-07-11 13:57:14 -0700

    ros-setup.sh script is much nicer to use now. Added ability to change distros inline.

--- conf/ros-setup.sh
+++ conf/ros-setup.sh
@@ -1,28 +1,47 @@
-# Grab the preferred ROS distribution.
-if [ -f $HOME/.rosdir ]; then
-	ROSDIR=`cat $HOME/.rosdir`
-else
-	for DIR in `ls -r /opt/ros`; do
-		if [ -f /opt/ros/$DIR/setup.sh ]; then
-			ROSDIR=/opt/ros/$DIR
-			break
-		fi
-	done
-fi
 
-if [ ! -d "$ROSDIR" ]; then
-	echo "No ROS distribution found."
-	return 1
-else
-	echo "Using ROS distro at: $ROSDIR"
-fi
+# This function can be used to switch between distros and workspaces.
+function rosselect {
+	if [[ -d $1 && -f $1/setup.sh ]]; then
+		echo $1 > $HOME/.rosdir
+	elif [[ -d /opt/ros/$1 && -f /opt/ros/$1/setup.sh ]]; then
+		echo /opt/ros/$1 > $HOME/.rosdir
+	else
+		echo "$1 setup files not found."
+		exit 1
+	fi
+	rossetup
+}
 
-# Environment variables for ROS.
-if [ -n "$ZSH_VERSION" ]; then
-	source $ROSDIR/setup.zsh
-elif [ -n "$BASH_VERSION" ]; then
-	source $ROSDIR/setup.bash
-else
-	source $ROSDIR/setup.sh
-fi
+# This function actually sources the setup file for the current distro.
+function rossetup {
+	# Grab the preferred ROS distribution, or fallback to a default.
+	if [ -f $HOME/.rosdir ]; then
+		ROSDIR=`cat $HOME/.rosdir`
+	else
+		for DIR in `ls -r /opt/ros`; do
+			if [ -f /opt/ros/$DIR/setup.sh ]; then
+				ROSDIR=/opt/ros/$DIR
+				break
+			fi
+		done
+	fi
 
+	# Did we find any ROS?
+	if [ ! -d "$ROSDIR" ]; then
+		echo "No ROS distribution found."
+		return 1
+	else
+		echo "Using ROS distro at: $ROSDIR"
+	fi
+
+	# Now call the actual setup script.
+	if [ -n "$ZSH_VERSION" ]; then
+		source $ROSDIR/setup.zsh
+	elif [ -n "$BASH_VERSION" ]; then
+		source $ROSDIR/setup.bash
+	else
+		source $ROSDIR/setup.sh
+	fi
+}
+
+rossetup
